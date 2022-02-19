319b5e39213efbc708224469e9a8b64c9c28ab23
fate84711 <fate84711@gmail.com>
2022-02-19 14:53:37 +0800

    add note

--- keyboard_matrix_v2/config.h
+++ keyboard_matrix_v2/config.h
@@ -3,6 +3,13 @@ const byte colPins[] = {14, 16};
 const byte analogpin[] = {A2};
 const int debounce_time = 10;
 
+// 要輸出的文字要使用' '包含 使用" " 會造成錯誤 
+//      col1 col2 ...
+//  row1
+//  row2
+//  row3
+//  ...
+
 const char keyMap[sizeof(rowPins)][sizeof(colPins)] = {
   {'q', 'a'},
   {'w', 's'},
